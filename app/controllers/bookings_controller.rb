617ee526e05e7038b0256b1c9cd01e19b1cef777
Komal Narrainen <komal.narrainen93@gmail.com>
2022-10-11 14:12:53 +0400

    Conflicts

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.cosplay = @cosplay
+    if @booking.save
+      redirect_to cosplay_path(@cosplay)
+    else
+      @cosplay = cosplay.new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to cosplay_path(@booking.cosplay), status: :see_other
+  end
 end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:cosplay_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
