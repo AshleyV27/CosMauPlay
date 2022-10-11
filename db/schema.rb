d6e5147c3e2b72861be08b19df51fb50dd8d3265
cedric sambat <cedricsambat9818@gmail.com>
2022-10-11 13:23:32 +0400

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,11 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2022_10_11_065941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -21,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_065941) do
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
+
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -32,11 +35,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_065941) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
+
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
+
   create_table "bookings", force: :cascade do |t|
     t.date "starting_date"
     t.date "ending_date"
@@ -47,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_065941) do
     t.index ["cosplay_id"], name: "index_bookings_on_cosplay_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
+
   create_table "cosplays", force: :cascade do |t|
     t.text "name"
     t.float "price"
@@ -55,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_065941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_065941) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "cosplays"
