25bfb2a884a7890b565b321e232d41c7b6a4106b
Ameenah Rostum <ameenah141991@gmail.com>
2022-10-12 16:24:52 +0400

    banner

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,42 @@ cosplays_bat.photo.attach(io: file,
                           filename: "Batman.jpg",
                           content_type: "image/jpg")
 cosplays_bat.save!
+
 cosplays_spi = Cosplay.new(name: "Spider", price: "1455", category: "cartoon", size: "medium")
+file = URI.open("https://i.etsystatic.com/19750816/r/il/b2956a/3571685226/il_fullxfull.3571685226_11n2.jpg")
+cosplays_spi.photo.attach(io: file,
+                          filename: "Spiderman.jpg",
+                          content_type: "image/jpg")
 cosplays_spi.save!
-# cosplays_ant = Cosplay.new(name: "Antman", price: "231", category: "cartoon", size: "small")
-# cosplays_ant.save!
-# cosplays_dea = Cosplay.new(name: "Deamon slayer", price: "654", category: "cartoon", size: "extra large")
-# cosplays_dea.save!
+
+cosplays_ant = Cosplay.new(name: "Antman", price: "231", category: "cartoon", size: "small")
+file = URI.open("https://www.kigurumisir.com/image/cache/catalog/cos/KSC20090503/0-880x880.jpg")
+cosplays_ant.photo.attach(io: file,
+                          filename: "Antman.jpg",
+                          content_type: "image/jpg")
+cosplays_ant.save!
+
+
+cosplays_dea = Cosplay.new(name: "Demons slayer", price: "654", category: "cartoon", size: "extra large")
+file = URI.open("https://d1flfk77wl2xk4.cloudfront.net/Assets/GalleryImage/82/666/L_g0118266682.jpg")
+cosplays_dea.photo.attach(io: file,
+                          filename: "DemonSplayer.jpg",
+                          content_type: "image/jpg")
+cosplays_dea.save!
+
 # cosplays_car = Cosplay.new(name: "Cars", price: "10000", category: "cartoon", size: "large")
+# file = URI.open("https://i.etsystatic.com/19750816/r/il/b2956a/3571685226/il_fullxfull.3571685226_11n2.jpg")
+# cosplays_spi.photo.attach(io: file,
+#                           filename: "Spiderman.jpg",
+#                           content_type: "image/jpg")
 # cosplays_car.save!
+
 # cosplays_nar = Cosplay.new(name: "Naruto", price: "85", category: "cartoon", size: "slim-fit")
 # cosplays_nar.save!
+
 # cosplays_dea = Cosplay.new(name: "Deadpool", price: "15", category: "cartoon", size: "xxxxL")
 # cosplays_dea.save!
-# # Examples:
+# # # Examples:
 # #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
