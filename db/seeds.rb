85f4ae9eacb93f3075c0cb14e43649d332b6c219
Komal Narrainen <komal.narrainen93@gmail.com>
2022-10-13 17:12:48 +0400

    updated img

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,34 +3,41 @@ Cosplay.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 cosplays_bat = Cosplay.new(name: "Batman", price: "100", category: "cartoon", size: "slim-fit 2xL")
-file = URI.open("https://static.wikia.nocookie.net/marvel_dc/images/4/4b/Batman_Vol_3_86_Textless.jpg/revision/latest?cb=20200502132734")
+file = URI.open("https://images.unsplash.com/photo-1588862081167-d5b98006637e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80")
 cosplays_bat.photo.attach(io: file,
                           filename: "Batman.jpg",
                           content_type: "image/jpg")
 cosplays_bat.save!
 
 cosplays_spi = Cosplay.new(name: "Spider", price: "1455", category: "cartoon", size: "medium")
-file = URI.open("https://i.etsystatic.com/19750816/r/il/b2956a/3571685226/il_fullxfull.3571685226_11n2.jpg")
+file = URI.open("https://images.unsplash.com/photo-1636569061713-0212bd19e3e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80")
 cosplays_spi.photo.attach(io: file,
                           filename: "Spiderman.jpg",
                           content_type: "image/jpg")
 cosplays_spi.save!
 
-cosplays_ant = Cosplay.new(name: "Antman", price: "231", category: "cartoon", size: "small")
-file = URI.open("https://www.kigurumisir.com/image/cache/catalog/cos/KSC20090503/0-880x880.jpg")
+cosplays_ant = Cosplay.new(name: "Harley quinn", price: "231", category: "cartoon", size: "small")
+file = URI.open("https://images.unsplash.com/photo-1553717188-e8b63785ee24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")
 cosplays_ant.photo.attach(io: file,
-                          filename: "Antman.jpg",
+                          filename: "Harley quinn.jpg",
                           content_type: "image/jpg")
 cosplays_ant.save!
 
 
-cosplays_dea = Cosplay.new(name: "Demons slayer", price: "654", category: "cartoon", size: "extra large")
-file = URI.open("https://d1flfk77wl2xk4.cloudfront.net/Assets/GalleryImage/82/666/L_g0118266682.jpg")
+cosplays_dea = Cosplay.new(name: "Jocker", price: "654", category: "cartoon", size: "extra large")
+file = URI.open("https://images.unsplash.com/photo-1568345889086-379dc9b8635b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80")
 cosplays_dea.photo.attach(io: file,
-                          filename: "DemonSplayer.jpg",
+                          filename: "Jocker.jpg",
                           content_type: "image/jpg")
 cosplays_dea.save!
 
+
+cosplays_dea = Cosplay.new(name: "Mulan", price: "654", category: "cartoon", size: "extra large")
+file = URI.open("https://images.unsplash.com/photo-1600385546605-536b691202df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=476&q=80")
+cosplays_dea.photo.attach(io: file,
+                          filename: "Mulan.jpg",
+                          content_type: "image/jpg")
+cosplays_dea.save!
 # cosplays_car = Cosplay.new(name: "Cars", price: "10000", category: "cartoon", size: "large")
 # file = URI.open("https://i.etsystatic.com/19750816/r/il/b2956a/3571685226/il_fullxfull.3571685226_11n2.jpg")
 # cosplays_spi.photo.attach(io: file,
