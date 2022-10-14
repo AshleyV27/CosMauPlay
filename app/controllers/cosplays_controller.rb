bf191b0124413b451fc5d201476961b3f303539b
Ashley Vengatasamy <vengatasamy.ashley@gmail.com>
2022-10-14 15:48:56 +0400

    conflicts

--- app/controllers/cosplays_controller.rb
+++ app/controllers/cosplays_controller.rb
@@ -14,7 +14,7 @@ class CosplaysController < ApplicationController
   def create
     @cosplay = Cosplay.new(cosplay_params)
     if @cosplay.save
-      redirect_to home_path(@cosplay)
+      redirect_to cosplays_path(@cosplay)
     else
       render :new, status: :unprocessable_entity
     end
